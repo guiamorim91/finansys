b2cda7a84ac4bb64f256ce055cba6b1cf58f7e04
Guilherme Amorim de Araújo <guilherme.araujo@engesoftware.com.br>
2021-02-15 15:57:50 -0300

    Formulario de categorias.

--- src/app/pages/categories/categories.module.ts
+++ src/app/pages/categories/categories.module.ts
@@ -1,19 +1,19 @@
-import { NgModule }     from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { NgModule }            from '@angular/core';
+import { CommonModule }        from '@angular/common';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { CategoriesRoutingModule } from './categories-routing.module';
 import { CategoryListComponent }   from './category-list/category-list.component';
 import { CategoryFormComponent }   from './category-form/category-form.component';
-import { CategoryService }         from './shared/category.service';
 
 @NgModule({
   declarations: [CategoryListComponent, CategoryFormComponent],
   imports: [
     CommonModule,
     CategoriesRoutingModule,
+    ReactiveFormsModule,
   ],
-  providers: [
-  ]
+  providers: [],
 })
 export class CategoriesModule {
 }
