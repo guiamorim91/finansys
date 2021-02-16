25151f9a23539d2b28cbccd28177794e211c7804
Guilherme Amorim de Araújo <guilherme.araujo@engesoftware.com.br>
2021-02-16 20:18:58 -0300

    Criação do componente header

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -3,21 +3,25 @@ import { CommonModule }        from '@angular/common';
 import { ReactiveFormsModule } from '@angular/forms';
 import { BreadcrumbComponent } from './components/breadcrumb/breadcrumb.component';
 import { RouterModule }        from '@angular/router';
+import { HeaderComponent }     from './components/header/header.component';
 
 @NgModule({
   imports: [
     CommonModule,
     ReactiveFormsModule,
-    RouterModule
+    RouterModule,
   ],
   declarations: [
-    BreadcrumbComponent
+    BreadcrumbComponent,
+    HeaderComponent,
   ],
   exports: [
     CommonModule,
     ReactiveFormsModule,
+    RouterModule,
     BreadcrumbComponent,
-    RouterModule
-  ]
+    HeaderComponent
+  ],
 })
-export class SharedModule { }
+export class SharedModule {
+}
