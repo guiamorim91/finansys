d9cad7581df4b8117ae36a83c53fb4ee66118ad2
Guilherme Amorim de Araújo <guilherme.araujo@engesoftware.com.br>
2021-02-16 21:37:44 -0300

    Criação do componente navbar.

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -5,6 +5,8 @@ import { BrowserAnimationsModule }        from '@angular/platform-browser/animat
 import { HttpClientModule }               from '@angular/common/http';
 import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
 import { InMemoryDatabase }               from '../in-memory-database';
+import { NavbarComponent }                from './components/navbar/navbar.component';
+import { RouterModule }                   from '@angular/router';
 
 @NgModule({
   imports: [
@@ -13,13 +15,15 @@ import { InMemoryDatabase }               from '../in-memory-database';
     BrowserAnimationsModule,
     HttpClientModule,
     HttpClientInMemoryWebApiModule.forRoot(InMemoryDatabase),
+    RouterModule,
   ],
-  declarations: [],
+  declarations: [NavbarComponent],
   exports: [
     BrowserModule,
     BrowserAnimationsModule,
     HttpClientModule,
     HttpClientInMemoryWebApiModule,
+    NavbarComponent,
   ],
 })
 export class CoreModule {
